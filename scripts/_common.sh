d82ba6b37d9cafb1a086a36c1e0eaf3637eae87b
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-09-02 13:39:30 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@ nodejs_version=16
 mongo_version=6.0
 
 _install_yarn () {
-    ynh_exec_as_app node_load_PATH" npm install yarn
+    ynh_exec_as_app npm install yarn
     ynh_yarn="$install_dir/node_modules/.bin/yarn"
     alias ynh_yarn="$ynh_yarn"
 }
