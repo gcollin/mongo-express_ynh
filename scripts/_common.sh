82e69f58f7cb917b78afa275887a70fad296ba61
Gérard Collin <gerard.collin@appliweb.net>
2024-09-15 11:55:21 +0200

    Fixed call to npm

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@ nodejs_version=20
 mongo_version=7.0
 
 _install_yarn () {
-    ynh_exec_as_app npm install yarn
+    ynh_exec_as_app $nodejs_dir/npm install yarn
     ynh_yarn="$install_dir/node_modules/.bin/yarn"
     alias ynh_yarn="$ynh_yarn"
 }
